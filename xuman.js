f66f044df63647cdef25733b01866a054d095f1d
Suman M <msuman04122003@gmail.com>
2024-06-10 23:04:19 +0530

    Add files via upload

---
+++ xuman.js
@@ -0,0 +1,15 @@
+  <script>
+    // JavaScript code
+    document.addEventListener('DOMContentLoaded', function() {
+      // Add smooth scrolling to all links
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          });
+        });
+      });
+    });
+  </script>
+
